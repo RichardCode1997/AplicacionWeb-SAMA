ca85fb02d6c415b25bc09dd97f34ee2637bf2cd1
Lniz <crce.aizu@gmail.com>
2023-05-14 02:40:45 -0500

    Rest Template ok Get Post Put Delete

---
+++ src/main/java/pe/edu/cibertec/proyectoswa2/samav1/configuration/RestTemplateConfig.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.proyectoswa2.samav1.configuration;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+}
